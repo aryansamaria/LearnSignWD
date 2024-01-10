6fce8c7947dd4b8d4e5f1c1b8988383d9cd83a2e
aryansamaria <aryansamaria44@gmail.com>
2024-01-10 10:33:13 +0530

    files

---
+++ minifield.js
@@ -0,0 +1,12 @@
+/*!
+ * jQuery v3.4.1
+ * (c) JS Foundation and other contributors
+ * jquery.org/license
+ */
+
+(function (global, factory) {
+    // ... [Minified code]
+ }(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
+    // ... [Minified code]
+ }));
+ 
